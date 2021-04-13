2c865ab0a310fd5ee0dfdc039652b20aead5034b
seolmin <wydos27@gmail.com>
2021-04-13 18:52:12 +0900

    input 여러개 관리하기_2.

--- src/inputSample.js
+++ src/inputSample.js
@@ -8,12 +8,30 @@ function InputSample(){
   })
   
   const {name, nickname}=inputs; // // 비구조화 할당을 통해 값 추출
+
+  const onChange = (e) =>{
+    setInputs()
+  }
+  const onReset = (e) =>{
+    setInputs()
+  }
+
   return(
     <div>
       <div>
-        <input name="name" value={name} placeholder="이름을 입력하시오"/>
-        <input name="nickname" value={nickname} placeholder="별명을 입력하시오" />
-        <button>초기화</button>
+        <input 
+          name="name" 
+          value={name} 
+          placeholder="이름을 입력하시오"
+          onChange={onChange}
+          />
+        <input 
+          name="nickname" 
+          value={nickname} 
+          placeholder="별명을 입력하시오"
+          onChange={onChange}
+           />
+        <button onClick={onReset}>초기화</button>
       </div>
         <p>값 : 이름(닉네임)</p>
     </div>
