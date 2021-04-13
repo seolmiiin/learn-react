d4a8e2dca7f6e044ad253c914112a7f1f4590425
seolmin <wydos27@gmail.com>
2021-04-13 16:36:20 +0900

    props_map()_add key props

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import Hello from './hello';
 import './App.css'
 
+
+//데이터에 id를 추가했다. 이걸 왜 해야할까? 리액트는 컴포넌트가 각각의 person이 다르다는 걸 알방법이 없기 때문에
+//리액트에게 컴포넌트가 서로 다르다는 것을 알려주는 방법이 컴포넌트에 key props를 추가하는 것
 const persons = [	{
+  id :1,
   name : 'seolmin',
   age : 32
 },
 {
+  id :2,
   name : 'hyosung',
   age : 29
 },
 {
+  id :3,
   name : 'sooin',
   age : 27
 }
 ];
 
 function renderPerson(person){
- return <Hello name={person.name} age={person.age} />
+ return <Hello key={person.id} name={person.name} age={person.age} />
 }
 
 function App() {
