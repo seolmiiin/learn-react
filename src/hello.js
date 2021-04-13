85d199bc7f536ca5f8ca36eae0053777888004c4
seolmin <wydos27@gmail.com>
2021-04-13 16:43:26 +0900

    props_Default Settings

--- src/hello.js
+++ src/hello.js
@@ -1,13 +1,26 @@
 import React from 'react';
 
-// 구조 분해 할당으로 props 사용하기
-function Hello({name, age}){
 
+function Hello({name, color}){
+  const style={
+    fontSizd :24,
+    color: color
+  }
   return(
     <>
-    <div> {name}님의 나이는 {age}입니다.</div>    
+    <div style={style}> 안녕하세요 {name} !</div>    
     </>
   );
+}
+//  defaultProps 로 기본값 설정
+
+// props를 지정하지 않았을때 기본적으로 사용할 값을 설정하고 싶을 때
+
+// 만약에 App.js에서 props을 빼먹었을때
+Hello.defaultProps={
+  color : "black",
+  name : "아무개"
+  
 }
 
 //내보내기
