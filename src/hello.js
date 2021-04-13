f1836475d2a33a69b9ba39eb3a036e983c4d139b
seolmin <wydos27@gmail.com>
2021-04-13 16:21:46 +0900

    props_Destructuring assignment

--- src/hello.js
+++ src/hello.js
@@ -1,14 +1,11 @@
 import React from 'react';
 
-//함수에서 props라는 프로퍼티 가져오기
-//props는 우리가 넣어준값들이 객체형태로 들어가있다.
-function Hello(props){
-  // const style={
-  //   color : props.color;
-  // }
+// 구조 분해 할당으로 props 사용하기
+function Hello({name, color}){
+
   return(
     <>
-    <div style={{color : props.color}}> {props.name}님 안녕하세요</div>
+    <div style={{color :color}}> {name}님 안녕하세요</div>
     {/* {{}} 객체를 감싸는 중괄호인것 */}
     </>
   );
