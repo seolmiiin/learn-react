8d032f8e941cddeb3c2e06cdd5f299badb8932ce
seolmin <wydos27@gmail.com>
2021-04-17 17:57:12 +0900

    Hooks_useEffect 2_Config Depth

--- src/userList.js
+++ src/userList.js
@@ -1,12 +1,21 @@
 import React, { useEffect } from 'react';
 
 function User({ user, onRemove, onToggle }) {
-  useEffect(()=>{
-    console.log('component가 화면에 나타남');
-    return ()=>{
-      console.log('component가 화면에서 사라짐')
-    }
-  },[]);
+  // useEffect(()=>{
+  //   console.log('component가 화면에 나타남');
+  //   return ()=>{
+  //     console.log('component가 화면에서 사라짐')
+  //   }
+  // },[]);
+  useEffect(() => {
+    console.log('user 값이 설정됨');
+    console.log(user);
+    return () => {
+      console.log('user 가 바뀌기 전..');
+      console.log(user);
+    };
+  }, [user]);
+
   return (
     <div>
       <b
