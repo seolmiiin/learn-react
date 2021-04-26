a79fa5b682c13b82257b37f3351aa7dc545717a3
seolmin <wydos27@gmail.com>
2021-04-26 15:02:55 +0900

    scss_Button Component

--- src/APP2.js
+++ src/APP2.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import Button from './components/Button';
-
+import './App2.scss';
 function APP2() {
   return (
     <div className="App">
-      <Button>BUTTON</Button>
+      <div className="Buttons">
+        <Button size="large">BUTTON</Button>
+        <Button color="yellow">BUTTON</Button>
+        <Button size="small" color="lime">
+          BUTTON
+        </Button>
+      </div>
+      <div className="Buttons">
+        <Button size="large" outline>
+          BUTTON
+        </Button>
+        <Button color="yellow" outline>
+          BUTTON
+        </Button>
+        <Button size="small" color="lime" outline>
+          BUTTON
+        </Button>
+      </div>
+      <div className="Buttons">
+        <Button fullWidth>BUTTON</Button>
+        <Button color="yellow" fullWidth>
+          BUTTON
+        </Button>
+        <Button color="lime" fullWidth>
+          BUTTON
+        </Button>
+      </div>
     </div>
   );
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import './Button.scss';
+import classNames from 'classnames';
 // yarn add node-sass
-
-function Button({ children }) {
+//BUTTON컴포넌트 만들기
+//1. color props
+//2. size
+//3. outline
+//4. fullWidth
+function Button({ children, color, size, outline, fullWidth }) {
   return (
     <>
-      <button className="Button">{children}</button>
+      <button
+        className={classNames('Button', color, size, {
+          outline,
+          fullWidth,
+        })}
+      >
+        {children}
+      </button>
     </>
   );
 }
-
+Button.defaultProps = {
+  color: 'orange',
+  size: 'medium',
+};
 export default Button;
